33c7c384f3c271ebf55719a0b75b1a29e506bc6b
Michael Jarman <mjarman5@gmail.com>
2020-04-19 22:27:12 -0400

    Added Token and Track data models

---
+++ iAttendTFL_WebApp/Models/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iAttendTFL_WebApp.Models
+{
+    public class token
+    {
+        public String token_hash { get; set; }
+        public String salt { get; set; }
+        public int account { get; set; }
+        public DateTime expiration_time { get; set; }
+        public bool isValid { get; set; }
+        public Char type { get; set; }
+    }
+}
---
+++ iAttendTFL_WebApp/Models/Track.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iAttendTFL_WebApp.Models
+{
+    public class Track
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public String type { get; set; }
+    }
+}
