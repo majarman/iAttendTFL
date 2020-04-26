b996f50ebfd75e91bf9b24bba92428f4d51040c8
duttmj <42615096+duttmj2019@users.noreply.github.com>
2020-04-25 20:38:58 -0400

    Lot of the code for creating an account has been added.

--- iAttendTFL_WebApp/Controllers/AccountsController.cs
+++ iAttendTFL_WebApp/Controllers/AccountsController.cs
@@ -109,6 +109,50 @@ namespace iAttendTFL_WebApp.Controllers
             return View(account);
         }
 
+        // POST: accounts/CreateAccount    --Matthew Dutt Version--
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAccount( string inputFirstName, string inputLastName, string inputEmail, string inputPassword, string inputConfirmPassword, string selectMonth, string selectYear, int selectTrack)
+        {
+
+            System.Diagnostics.Debug.WriteLine("Hello world");
+            if (inputEmail.EndsWith("@mountunion.edu"))
+            {
+                if (!emailExists(inputEmail))
+                {
+                    if (meetsComplexityRequirement(inputPassword))
+                    {
+                        if (inputPassword == inputConfirmPassword)
+                        {
+                            DateTime gradDate = formatDate(selectMonth, selectYear); //takes string values from dropdown list selections, combines them, and creates a DateTime variable with that data.
+                            account tempAccount = new account();
+                            //add id increment handling stuff in web app in the future instead of db
+                            tempAccount.id = 3001;
+                            tempAccount.first_name = inputFirstName;
+                            tempAccount.last_name = inputLastName;
+                            tempAccount.email = inputEmail;
+                            tempAccount.password_hash = inputPassword;
+                            tempAccount.expected_graduation_date = gradDate;
+                            tempAccount.track_id = selectTrack;
+                            tempAccount.salt = "DefaultSalt";
+                                if (ModelState.IsValid)
+                                {
+                                    _context.Add(tempAccount);
+                                    _context.SaveChangesAsync();
+                                    return RedirectToAction(nameof(Index));
+                                }
+                                return View(tempAccount);
+                                //return view();
+                            
+                        } return NotFound(); //password match
+                    } return NotFound(); //password meets complexity requirements
+                } return NotFound(); //email does not exist
+            } return NotFound(); //email suffix is @mountunion.edu
+
+        } //
+
         // GET: accounts/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -189,9 +233,44 @@ namespace iAttendTFL_WebApp.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //==========================
+        //ustilized custom functions
+        //==========================
+
         private bool accountExists(int id)
         {
             return _context.account.Any(e => e.id == id);
         }
+
+        private bool emailExists(string userEmail)
+        {
+            return _context.account.Any(e => e.email == userEmail);
+        }
+
+        private bool meetsComplexityRequirement(string userPassword)
+        {
+            if ( userPassword.Length > 7 && userPassword.Any(char.IsDigit) && userPassword.Any(char.IsUpper) && userPassword.Any(char.IsSymbol) )
+            {
+                return true;
+            } else { return false; }
+        }
+        
+        private DateTime formatDate(string userMonth, string userYear)
+        {
+            string format = "yyyyMMdd";
+            string dateString = userYear + userMonth + "28";
+            
+            DateTime formattedEGD;
+            formattedEGD = DateTime.Parse(dateString);
+            //formattedEGD = DateTime.TryParseExact(dateString, format, null,
+                               //System.Globalization.DateTimeStyles.AllowWhiteSpaces |
+                               //System.Globalization.DateTimeStyles.AdjustToUniversal,
+                               //out formattedEGD);           
+            System.Diagnostics.Debug.WriteLine("====formattedEGD====");
+            System.Diagnostics.Debug.WriteLine(formattedEGD.ToString());
+            System.Diagnostics.Debug.WriteLine("====formattedEGD====");
+            return formattedEGD;
+        }
+
     }
 }
