c913c7ffa318dfc2e2acd0cb7d4d0ea5f90165af
duttmj <42615096+duttmj2019@users.noreply.github.com>
2020-04-27 00:13:57 -0400

    Basic Registration Complete
    FINALLY got the registration done.
    This has password and email validation checks.
    It is "basic" because it is only part 1, but without any of the email verification included, which makes up the finale of part 1 and all of part 2. Part 3 is not included becaise it will also use future email creation and sending knowledge.

--- iAttendTFL_WebApp/Controllers/AccountsController.cs
+++ iAttendTFL_WebApp/Controllers/AccountsController.cs
@@ -122,130 +122,49 @@ namespace iAttendTFL_WebApp.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("first_name,last_name,email,salt,password_hash,account_type,track_id")] account account)
+        public async Task<IActionResult> Create([Bind("first_name,last_name,email,salt,password_hash,password_confirm,gradMonth,gradYear,account_type,track_id")] account account)
         {
-            if (account.email.EndsWith("@mountunion.edu"))
+                
+            if (ModelState.IsValid)
             {
-                if (!emailExists(inputEmail))
-                {
-                    // if (meetsComplexityRequirement(inputPassword))
-                    // {
-                        if (inputPassword == inputConfirmPassword)
-                        {
-                            if (ModelState.IsValid)
+                //if (account.email.EndsWith("@mountunion.edu"))
+                //{
+                    if (!emailExists(account.email))
+                    {
+                        //if (meetsComplexityRequirement(account.password_hash))
+                        //{
+                            if (account.password_hash == account.password_confirm)
                             {
+                                account.account_type = 's';
                                 account.salt = "ab";
+                                account.email_verified = true;
                                 account.barcode = Encoding.UTF8.GetBytes("TEST");
-                                account.expected_graduation_date = new DateTime(2020, 05, 01);
+                                account.expected_graduation_date = formatDate(account.gradMonth, account.gradYear);
+                                //account.expected_graduation_date = new DateTime(2020, 05, 16);
                                 //account.pushBarcode(123456);
                                 _context.Add(account);
                                 await _context.SaveChangesAsync();
-
-
                                 return RedirectToAction(nameof(Index));
-                            }
-                        }
-                    // }
-                }
+                            
+                            } // passwords match
+                            else { return RedirectToAction(nameof(Create)); }
+                        
+                        //} //complexity requirement
+                        //else { return RedirectToAction(nameof(Create)); }
+                    
+                        //return RedirectToAction(nameof(Index));
+
+                    } // email does not already exist in database
+                    else { return RedirectToAction(nameof(Create)); }
+
+                //return RedirectToAction(nameof(Index));
+                //}
             }
+
             
             return View(account);
         }
 
-        // POST: accounts/CreateAccount    --Matthew Dutt Version--
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateAccount( string inputFirstName, string inputLastName, string inputEmail, string inputPassword, string inputConfirmPassword, string selectMonth, string selectYear, int selectTrack)
-        {
-
-            System.Diagnostics.Debug.WriteLine("Hello world");
-            if (inputEmail.EndsWith("@mountunion.edu"))
-            {
-                Console.WriteLine("1");
-                if (!emailExists(inputEmail))
-                {
-                    Console.WriteLine("2");
-                    //if (meetsComplexityRequirement(inputPassword))
-                   // {
-                        Console.WriteLine("3");
-                        if (inputPassword == inputConfirmPassword)
-                        {
-                            Console.WriteLine("4");
-                            DateTime gradDate = formatDate(selectMonth, selectYear); //takes string values from dropdown list selections, combines them, and creates a DateTime variable with that data.
-                            account tempAccount = new account();
-                            //add id increment handling stuff in web app in the future instead of db
-                            //tempAccount.id = 3001;
-                            char[] t1 = inputFirstName.ToCharArray();
-                        foreach(char i in t1)
-                        {
-                            Console.WriteLine(i);
-                            if (i.Equals('\0') || i.Equals(null))
-                            {
-                                Console.WriteLine("BADTHINGHERE");
-                            }
-                        }
-                            char[] t2 = inputLastName.ToCharArray();
-                        foreach (char i in t2)
-                        {
-                            Console.WriteLine(i);
-                            if (i.Equals('\0') || i.Equals(null))
-                            {
-                                Console.WriteLine("BADTHINGHERE");
-                            }
-                        }
-                        char[] t3 = inputEmail.ToCharArray();
-                        foreach (char i in t3)
-                        {
-                            Console.WriteLine(i);
-                            if (i.Equals('\0') || i.Equals(null))
-                            {
-                                Console.WriteLine("BADTHINGHERE");
-                            }
-                        }
-                        char[] t4 = inputPassword.ToCharArray();
-                        foreach (char i in t4)
-                        {
-                            Console.WriteLine(i);
-                            if (i.Equals('\0') || i.Equals(null))
-                            {
-                                Console.WriteLine("BADTHINGHERE");
-                            }
-                        }
-                        char[] t5 = gradDate.Date.ToString().ToCharArray();
-                        foreach (char i in t5)
-                        {
-                            Console.WriteLine(i);
-                            if (i.Equals('\0') || i.Equals(null))
-                            {
-                                Console.WriteLine("BADTHINGHERE");
-                            }
-                        }
-
-                        tempAccount.first_name = inputFirstName.Replace("s/\x00//g", "").Replace("\u0000", "");
-                            tempAccount.last_name = inputLastName.Replace("s/\x00//g", "").Replace("\u0000", "");
-                            tempAccount.email = inputEmail.Replace("s/\x00//g", "").Replace("\u0000", "");
-                            tempAccount.password_hash = inputPassword.Replace("s/\x00//g", "").Replace("\u0000", "");
-                            tempAccount.expected_graduation_date = gradDate;
-                            tempAccount.track_id = selectTrack;
-                            tempAccount.salt = "DefaultSalt".Replace("s/\x00//g", "");
-                            tempAccount.barcode = Encoding.UTF8.GetBytes("TEST".Replace("s/\x00//g", ""));
-                            if (ModelState.IsValid)
-                            {
-                                Console.WriteLine("5");
-                                _context.Add(tempAccount);
-                                await _context.SaveChangesAsync();
-                                return RedirectToAction(nameof(Index));
-                            }
-                            return View("Index");
-                                //return view();
-                            
-                        } return NotFound(); //password match
-                   // } return NotFound(); //password meets complexity requirements
-                } return NotFound(); //email does not exist
-            } return NotFound(); //email suffix is @mountunion.edu
-        } //
 
         // GET: accounts/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -343,10 +262,11 @@ namespace iAttendTFL_WebApp.Controllers
 
         private bool meetsComplexityRequirement(string userPassword)
         {
-            if ( userPassword.Length > 7 && userPassword.Any(c => char.IsDigit(c)) && userPassword.Any(c => char.IsUpper(c)) && userPassword.Any(c => char.IsSymbol(c)))
+            if (userPassword.Length > 7 && userPassword.Any(c => char.IsDigit(c)) && userPassword.Any(c => char.IsUpper(c)) && userPassword.Any(c => char.IsSymbol(c)))
             {
                 return true;
-            } else { return false; }
+            }
+            else { return false; }
         }
         
         private DateTime formatDate(string userMonth, string userYear)
--- iAttendTFL_WebApp/Models/Account.cs
+++ iAttendTFL_WebApp/Models/Account.cs
@@ -5,6 +5,8 @@ using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using BarcodeLib;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace iAttendTFL_WebApp.Models
 {
@@ -17,9 +19,17 @@ namespace iAttendTFL_WebApp.Models
         public string last_name { get; set; }
         public string email { get; set; }
         public string salt { get; set; }
+
+        [StringLength(200, ErrorMessage = "The password must be at least 8 characters long.", MinimumLength = 8)]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Passwords must be at least 8 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         public string password_hash { get; set; }
+
+        [NotMapped] public string password_confirm { get; set; }
+
         public char account_type { get; set; }
         public bool email_verified { get; set; } = false;
+        [NotMapped] public string gradMonth { get; set; }
+        [NotMapped] public string gradYear { get; set; }
         public DateTime expected_graduation_date { get; set; }
         public int track_id { get; set; }
         public byte[] barcode { get; set; }
@@ -53,7 +63,6 @@ namespace iAttendTFL_WebApp.Models
         {
             this.barcode = makeTheBarcode(id);
         }
-
 
 
 
