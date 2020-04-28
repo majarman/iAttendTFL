25be186191714bf557751b9b3b6bed20903c9690
duttmj <42615096+duttmj2019@users.noreply.github.com>
2020-04-28 01:49:51 -0400

    Feeling good about it. Finally.

--- iAttendTFL_MobileApp/iAttendTFL_MobileApp/MainPage.xaml.cs
+++ iAttendTFL_MobileApp/iAttendTFL_MobileApp/MainPage.xaml.cs
@@ -4,24 +4,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
 
 namespace iAttendTFL_MobileApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        private Button buttonScan;
+
         public MainPage()
         {
+            //ZXingScannerPage scanPage = new ZXingScannerPage();
+            //await Navigation.PushAsync(scanPage);
+
+            //buttonScan.Click += (sender, e) =>
+            //{
+
+            //        #if __ANDROID__
+	           //     // Initialize the scanner first so it can track the current context
+	           //     MobileBarcodeScanner.Initialize (Application);
+            //        #endif
+
+            //    var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            //    var result = await scanner.Scan();
+
+            //    if (result != null)
+            //        Console.WriteLine("Did you mean to scan " + result.Text + "?");
+            //};
+
             InitializeComponent();
         }
-        void OnSubmitButtonClicked(object sender, EventArgs eventArgs)
+
+        public void Handle_OnScanResult(Result result)
         {
-
-            Submission sendme = new Submission(int.Parse(idEditor.Text), nameEditor.Text);
-
+            //TODO: I think here or the layer below is where the code to find the user and add them into the event will go.
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("account.first_name +sp+ account.last_name", result.Text, "OK");
+            });
         }
+        //void OnSubmitButtonClicked(object sender, EventArgs eventArgs)
+        //{
+
+        //    Submission sendme = new Submission(int.Parse(idEditor.Text), nameEditor.Text);
+
+        //}
+
+
     }
 }
